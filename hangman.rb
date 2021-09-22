a52861de90982c98f5ee28e156cbceb964cea760
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-09-22 18:05:30 +0300

    Created the Hangman class that creates the board, takes the player input and compares it against the secret word updating the board accordingly.

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,9 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require 'pry-byebug'
-class SECRET_WORD
+
+# Picks a random word out of a given word list
+class SecretWord
   attr_reader :word
 
   def initialize
@@ -16,7 +18,7 @@ class SECRET_WORD
   def random_word
     word = find_word_at_line(random_line_index)
     word = random_word if verify_word(word) == false
-    word
+    word.downcase
   end
 
   def verify_word(word)
@@ -31,4 +33,64 @@ class SECRET_WORD
     end
   end
 end
-word = SECRET_WORD.new
+
+# the core game i guess
+class Hangman
+  def initialize(secret_word)
+    @secret_word = secret_word
+    board_length = secret_word.length
+    @board = Array.new(board_length) { '_' }
+    @correct_guesses = []
+    @incorrect_guesses = []
+  end
+
+  def display_board
+    temp_board = @board.join(' ')
+    puts "Correct guesses: #{@correct_guesses}"
+    puts "Incorrect guesses: #{@incorrect_guesses}"
+    temp_board
+  end
+
+  def player_input
+    # get player input
+    player_input = gets.chomp
+    # validate player input
+    player_input unless player_input_valid?(player_input) == true
+    update_board(compare_player_input(player_input), player_input)
+    display_board
+  end
+
+  def player_input_valid?(player_input)
+    test_results = []
+    test_results << player_input.length == 1
+    test_results << /^[a-z]+$/i.match?(player_input)
+    test_results.all?(true)
+  end
+
+  def compare_player_input(player_input)
+    /[#{player_input}]/i.match?(@secret_word)
+  end
+
+  def update_board(comparison_results, player_input)
+    if comparison_results == false
+      @incorrect_guesses << player_input
+    else
+      @correct_guesses << player_input
+      string_to_array(@secret_word).each_with_index do |char, index|
+        @board[index] = char if char == player_input
+      end
+    end
+  end
+
+  def string_to_array(string)
+    result_array = []
+    string.each_char do |char|
+      result_array << char
+    end
+    result_array
+  end
+end
+
+binding.pry
+
+something = 'something'
